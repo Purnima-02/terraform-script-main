1b8ac4937b43e9be744aa41fd0ff638922076e40
Purnima-02 <140998496+Purnima-02@users.noreply.github.com>
2024-09-06 13:52:15 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get -y update
 sudo apt-get install -y python3-pip
-git clone https://github.com/GOUSERABBANI44/Penguin-.git
-cd Penguin-/
+git clone https://github.com/Purnima-02/php.git
+cd Php/
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
